1d9129dc76398a07efdbf5d0dd4460d788095efe
Gerhard Unger <gerhard.unger@tugraz.at>
2024-10-23 13:22:39 +0200

    [#179] Remove not needed dependencies

--- version.php
+++ version.php
@@ -31,7 +31,6 @@ $plugin->component    = 'lytix_activity'; // Full name of the plugin.
 $plugin->dependencies = [
     'lytix_helper'          => ANY_VERSION,
     'lytix_logs'            => ANY_VERSION,
-    'lytix_config'          => ANY_VERSION,
 ];
 $plugin->release   = 'v1.0.8';
 $plugin->supported = [401, 405];
